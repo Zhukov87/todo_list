97a2c19cc36fcea6adc3679885605f2fa6b14a7d
Zhukov87 <boxbriefe@gmail.com>
2020-02-19 18:23:51 +0300

    save state in localStorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import AddForm from './components/AddForm';
-import Filter from './components/Filter';
-import TasksList from './components/TasksList';
+import AddForm from './components/AddForm/';
+import TasksList from './components/TasksList/';
+import Filter from './components/Filter/';
 import { Provider } from 'react-redux';
-import store from './store';
+import storeConfig from './store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
+  const { store, persistor } = storeConfig();
+  
   return (
     <Provider store={store}>
-      <AddForm />
-      <Filter />
-      <TasksList />
+      <PersistGate loading={null} persistor={persistor}>
+        <AddForm />
+        <Filter />
+        <TasksList />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,20 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from '../ducks';
 import middleware from '../middleware';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(middleware)));
-
-export default store;
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+  
+const persistedReducer = persistReducer(persistConfig, reducer);
+  
+export default () => {
+    let store = createStore(persistedReducer, composeEnhancer(applyMiddleware(middleware)));
+    let persistor = persistStore(store);
+    return { store, persistor };
+};

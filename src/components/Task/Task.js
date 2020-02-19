4e6a10646f0b377c8f6ca5eb3bf05d291622e3b9
Zhukov87 <boxbriefe@gmail.com>
2020-02-19 17:36:17 +0300

    added working TasksList

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 const Task = (props) => {
+    const {title, date, isOpen} = props;
+    
     return (
-        <div>
-            Task
-        </div>
+        <li>
+            {isOpen ? (
+                <div>
+                    <p>Close task</p>
+                    <input type='checkbox' value={isOpen} ></input>
+                </div>
+                ) : <p>Task is closed</p>}
+                <h5>{title}</h5>
+                <p>{date}</p>
+                <button >Edit</button>
+                <button >Remove</button>
+        </li>
     );
 }
 
--- src/components/TasksList/TasksList.js
+++ src/components/TasksList/TasksList.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Task from '../Task';
+import { useSelector } from 'react-redux';
 
 const TasksList = () => {
+    const tasks = useSelector(state => state.tasks);
+
+    const renderList = () => {
+        return (
+            <ul>
+                {tasks.map(task => {
+                    return <Task 
+                                key={task.id}
+                                title={task.title} 
+                                date={task.creationDate} 
+                                id={task.id}
+                                isOpen={task.isOpen} 
+                            />
+                })}
+            </ul>
+        );
+    }
+
     return (
-        <ul>
-        </ul>
+        tasks.length ? renderList() : <p>Add a first task!</p>
     );
 }
 
